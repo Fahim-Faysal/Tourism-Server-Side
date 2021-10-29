b6c959efeb5ba70feb79b08de625c48e87500c60
Fahim-Faysal <faysalsiyam@gmail.com>
2021-10-29 21:35:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+const { MongoClient } = require('mongodb')
+const ObjectId = require('mongodb').ObjectId
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 4000;
+
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yeroo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+      try {
+            await client.connect()
+
+            const database = client.db("travelling")
+            const travelCollection = database.collection("packages")
+
+
+            app.get('/package', async (req, res) => {
+
+                  const cursor = await travelCollection.find({})
+                  const result = await cursor.toArray()
+
+                  res.send(result)
+            })
+
+            app.get('/details/:id', async (req, res) => {
+                  const id = req.params.id
+
+                  const query = { _id: ObjectId(id) }
+                  const result = await travelCollection.findOne(query)
+                  console.log(result);
+                  res.send(result)
+            })
+
+      }
+      finally {
+            // await client.close()
+      }
+}
+run().catch(console.dir)
+
+
+app.get('/', async (req, res) => {
+      res.send('assignment')
+})
+
+app.listen(port, () => {
+      console.log('Port is running on :', port);
+})
